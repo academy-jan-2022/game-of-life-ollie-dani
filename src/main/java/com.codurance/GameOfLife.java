31072579a9e4ff7daf46e381d7d1988e6b790e3d
oawood93 <oa.wood@outlook.com>
2022-01-31 16:02:59 +0000

    green

--- src/main/java/com.codurance/GameOfLife.java
+++ src/main/java/com.codurance/GameOfLife.java
@@ -1,5 +1,7 @@
 package com.codurance;
 
+import java.util.Arrays;
+
 public class GameOfLife {
     private final int[][] board;
 
@@ -8,10 +10,18 @@ public class GameOfLife {
     }
 
     public int[][] nextGen() {
+        var nextBoard = Arrays.stream(board).map(int[]::clone).toArray(int[][]::new);
+
         for(int row = 0; row < board[0].length; row++){
-            board[0][row] = 0;
+
+            if (row == 1 && board[0][row] == 1 && board[0][row - 1] == 1){
+                nextBoard[0][row] = 1;
+                continue;
+            }
+
+            nextBoard[0][row] = 0;
         }
 
-        return board;
+        return nextBoard;
     }
 }
