6fa0e60ba405fa395d44c348ce21f4f62f02b4ae
oawood93 <oa.wood@outlook.com>
2022-01-31 14:54:39 +0000

    Revert "green - red"
    This reverts commit a95af065d227e81d79a4e1f9b0b1b948f16ff27e.

--- src/main/java/com.codurance/GameOfLife.java
+++ src/main/java/com.codurance/GameOfLife.java
@@ -1,18 +1,10 @@
 package com.codurance;
 
 public class GameOfLife {
-    private final boolean[][] board;
-
     public GameOfLife(boolean[][] initialState) {
-        board = initialState;
     }
 
     public boolean[][] nextGen() {
-        for(int column = 0 ; column < board[0].length ; column ++){
-            for(int row = 0 ; row < board.length; row ++){
-                board[column][row] = false;
-            }
-        }
-        return board;
+        return new boolean[][]{{ false, false }, { false, false }};
     }
 }
