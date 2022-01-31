a95af065d227e81d79a4e1f9b0b1b948f16ff27e
danihra-codurance <daniel.rodriguez@codurance.com>
2022-01-31 14:55:46 +0100

    green - red

--- src/main/java/com.codurance/GameOfLife.java
+++ src/main/java/com.codurance/GameOfLife.java
@@ -1,10 +1,18 @@
 package com.codurance;
 
 public class GameOfLife {
+    private final boolean[][] board;
+
     public GameOfLife(boolean[][] initialState) {
+        board = initialState;
     }
 
     public boolean[][] nextGen() {
-        return new boolean[][]{{ false, false }, { false, false }};
+        for(int column = 0 ; column < board[0].length ; column ++){
+            for(int row = 0 ; row < board.length; row ++){
+                board[column][row] = false;
+            }
+        }
+        return board;
     }
 }
